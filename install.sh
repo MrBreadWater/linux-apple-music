1ec2261f6fb6190ef424983ef8ffba44eeaa4a09
Michael Paniagua <mrbreadwater@yahoo.com>
2020-07-24 23:31:20 -0700

    Update install script to delete tar file

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ INSTALL_DIR=$PD"/AppleMusic"
 echo $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 tar xvf $PD/AppleMusic-linux-x64.tar.xz -C $INSTALL_DIR --strip 1
+rm $PD/AppleMusic-linux-x64.tar.xz
 /bin/bash $PD/create_shortcut.sh
